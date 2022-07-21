43b5acb80b3b5de8b7be7165a558094ffd1b8694
Jesse <code@seqmanagement.com>
2022-07-21 12:19:05 -0700

    Initial Commit for task_services

---
+++ xer_pro/services/task_services.py
@@ -0,0 +1,32 @@
+from data.task import Task
+
+ADMIN_VERBS = (
+    'submit', 'submittal', 'shop drawing', 'product data', 'review', 'approve', 'approval',
+    'procure', 'procurement', 'fabricate', 'lead time', 'deliver', 'obtain',
+    'buyout', 'purchase', 'coordination', 'coordinate', 'allowance', 'closeout')
+
+CONSTRUCTION_VERBS = (
+    'install', 'erect', 'swing', 'set', 'pour', 'place', 'form', 'layout',
+    'excavate', 'dig', 'rough in', 'rough-in')
+
+
+def is_construction_task(task: Task) -> bool:
+    """Determine if Task is construction work.
+
+    Args:
+        task (Task): Schedule activity
+
+    Returns:
+        bool: True if is Construction; False if Administrative or Procurement
+    """
+    for node in task.wbs.path(include_proj_node=False):
+        if any(verb in node.name.lower() for verb in ADMIN_VERBS):
+            return False
+
+    if task.name.lower().startswith(CONSTRUCTION_VERBS):
+        return True
+
+    if any(verb in task.name.lower() for verb in ADMIN_VERBS):
+        return False
+
+    return True
