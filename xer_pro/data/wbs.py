dd1d6aa4f21f8de65cdf6ea20262cb373fa77346
Jesse <code@seqmanagement.com>
2022-07-21 12:31:26 -0700

    Added additional properties and methods; added docstring

--- xer_pro/data/wbs.py
+++ xer_pro/data/wbs.py
@@ -1,10 +1,56 @@
-class Wbs:
+
+class WbsNode:
+    """A class to represent a schedule WBS node.
+
+    ...
+
+    Attributes
+    ----------
+    name: str
+        Long name for WBS node
+    short_name: str
+        Short name for WBS node
+    is_project_node: bool
+        Flags if node is Project Node
+
+    ...
+
+    Methods
+    ----------
+    path: list
+        Returns list of WBS node and all parent nodes
+    """
     def __init__(self, **kwargs) -> None:
         self._attr = kwargs
+        self.parent = None
 
     def __getitem__(self, name: str):
         return self._attr[name]
 
     @property
+    def name(self) -> str:
+        return self._attr['wbs_name']
+
+    @property
+    def short_name(self) -> str:
+        return self._attr['wbs_short_name']
+
+    @property
     def is_project_node(self) -> bool:
         return self._attr['proj_node_flag']
+
+    def path(self, include_proj_node: bool = True) -> list:
+        if self.is_project_node:
+            return self._attr['short_name']
+
+        if not self.parent:
+            return []
+
+        wbs_node = self
+        path = []
+        while wbs_node:
+            if wbs_node.is_project_node == include_proj_node:
+                path.append(wbs_node)
+            wbs_node = wbs_node.parent
+
+        return reversed(path)
